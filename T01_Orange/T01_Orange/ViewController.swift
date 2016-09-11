fb98c38440ac8aee6e461c70474a68be6b1b94b8
NerdAlert58 <nerd@unknownacbc32cdd833.attlocal.net>
2016-09-11 18:24:36 -0500

    Created Project in Xcode-beta4

---
+++ T01_Orange/T01_Orange/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  T01_Orange
+//
+//  Created by Scott Bushyhead on 9/11/16.
+//  Copyright © 2016 Scott Bushyhead. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

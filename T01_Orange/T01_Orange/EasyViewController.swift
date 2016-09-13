c702d0620667bd13433b7b3bd55e742d0537e03e
NerdAlert58 <nerd@unknownacbc32cdd833.attlocal.net>
2016-09-13 18:17:40 -0500

    Created view controllers and segues

---
+++ T01_Orange/T01_Orange/EasyViewController.swift
@@ -0,0 +1,35 @@
+//
+//  EasyViewController.swift
+//  T01_Orange
+//
+//  Created by Scott Bushyhead on 9/13/16.
+//  Copyright © 2016 Scott Bushyhead. All rights reserved.
+//
+
+import UIKit
+
+class EasyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- T01_Orange/T01_Orange/ViewController.swift
+++ T01_Orange/T01_Orange/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblSplash: UILabel!
+    @IBOutlet weak var btnStart: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.

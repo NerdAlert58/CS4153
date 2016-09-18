dc57566bc5551449edc1176775fd36354340485c
NerdAlert58 <nerd@unknownacbc32cdd833.attlocal.net>
2016-09-18 13:44:37 -0500

    Adding Puzzle Class -Scott

---
+++ T01_Orange/T01_Orange/Puzzle.swift
@@ -0,0 +1,42 @@
+//
+//  Puzzle.swift
+//  T01_Orange
+//
+//  Created by Scott Bushyhead on 9/18/16.
+//  Copyright © 2016 Scott Bushyhead. All rights reserved.
+//
+
+import Foundation
+
+class Puzzle {
+    var p: [String] = ["1","2","3","4","5","6","7","8",""]
+    var blankPosition = 8
+    
+    func scramble(input: [String]) -> [String]{
+        let output: [String] = input
+        return output
+    }
+    
+    func moveLeft(){
+        
+    }
+    
+    func moveRight(){
+        
+    }
+    
+    func moveUp(){
+        
+    }
+    
+    func moveDown(){
+        
+    }
+    
+    func rules(){
+        
+    }
+    
+    
+    
+}
